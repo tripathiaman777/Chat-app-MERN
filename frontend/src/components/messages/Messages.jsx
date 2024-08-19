d7399aee7318db8796f716d4a4ac1a45831c1e5a
Amanpati Tripathi <tripathiaman777@gmail.com>
2024-08-20 01:57:00 +0530

    MessageSkeleton is corrected

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from "react";
 import useGetMessages from "../../hooks/useGetMessages.js";
-import MessageSkeleton from "../skeletons/messageSkeleton.jsx";
+import MessageSkeleton from "../skeletons/MessageSkeleton.jsx";
 import Message from "./Message.jsx";
 import useListenMessages from "../../hooks/useListenMessages.js";
 
