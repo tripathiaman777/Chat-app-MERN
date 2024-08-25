5a697395e578f7123739e6d30f0681b65a083be0
Amanpati Tripathi <tripathiaman777@gmail.com>
2024-08-25 16:16:33 +0530

    server.js modified

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,18 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
+import path from "path";
 import authRoute from "./routes/auth.route.js";
 import messageRoute from "./routes/message.route.js";
 import userRoute from "./routes/user.route.js";
-import path from "path";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
+
 dotenv.config();
+
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
-// const app = express();
+
 app.use(
   cors({
     origin: "http://localhost:3000", // allow requests from this origin
@@ -20,20 +21,24 @@ app.use(
     credentials: true, // if you need to handle cookies
   })
 );
-// app.get("/", (req, res) => {
-//   res.send("Hello World!!");
-// });
+
 app.use(express.json());
 app.use(cookieParser());
-app.use("/api/auth", authRoute); //middleware
-app.use("/api/users", userRoute); //middleware
-app.use("/api/messages", messageRoute); //middleware
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// Routes
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/messages", messageRoute);
+
+// Serve static files
+app.use(express.static(path.join(__dirname, "frontend", "dist")));
+
+// Handle all other routes by serving index.html
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend","dist","index.html"));
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
+// Start server
 server.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server is running on port ${PORT}`);
