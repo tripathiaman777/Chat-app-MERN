95b9fe4b5aac6f6f64a758ecd9d2f6264d6bba84
Amanpati Tripathi <tripathiaman777@gmail.com>
2024-08-25 17:18:17 +0530

    try at 4000 port for frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 4000,
     proxy: {
       "/api": {
         target: "https://chat-app-mern-1-941d.onrender.com/",
