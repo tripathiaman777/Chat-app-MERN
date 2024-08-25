e9999521ca1c4332dc024285f0eebdd9aa0e5189
Amanpati Tripathi <tripathiaman777@gmail.com>
2024-08-25 16:36:50 +0530

    backend api changed in frontend repo

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "https://chat-app-mern-1-941d.onrender.com/",
         changeOrigin: true,
       },
     },
